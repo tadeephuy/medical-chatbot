e6bd0a3d5fa16da2c30d3e2a2ec6171acab0e984
demdecuong <nguyenphucminh2804@gmail.com>
2021-04-21 13:04:51 +0700

    merge deploy + nlu_api

--- code/inference.py
+++ code/inference.py
@@ -13,14 +13,16 @@ from transformers import RobertaConfig, AutoTokenizer
 from model.model import JointphoBERT
 from dataset import convert_input_file_to_tensor_dataset
 
+from kb.mapper import Mapper
+
 def load_tokenizer():
     return AutoTokenizer.from_pretrained("vinai/phobert-base")
 
 def get_intent_labels():
-    return [label.strip() for label in open("intent_label.txt", 'r', encoding='utf-8')]
+    return [label.strip() for label in open("../data/intent_label.txt", 'r', encoding='utf-8')]
 
 def get_slot_labels():
-    return [label.strip() for label in open("slot_label.txt", 'r', encoding='utf-8')]
+    return [label.strip() for label in open("../data/slot_label.txt", 'r', encoding='utf-8')]
 
 def get_args(arg_folder):
     return torch.load(os.path.join(arg_folder, 'training_args.bin'))
@@ -32,16 +34,16 @@ class NLUprocess:
     def __init__(self, arg_folder):
         self.args = get_args(arg_folder)
         self.device = get_device()
-        self.model = JointphoBERT.from_pretrained(self.args.model_dir,\
-                                                    args=self.args,\
-                                                    intent_label_lst=get_intent_labels(),\
-                                                    slot_label_lst=get_slot_labels())
-        self.model.to(self.device)
-        self.model.eval()
 
         self.intent_label_lst = get_intent_labels()
         self.slot_label_lst = get_slot_labels()
-
+        
+        self.model = JointphoBERT.from_pretrained(self.args.model_dir,\
+                                                    args=self.args,\
+                                                    intent_label_lst=self.intent_label_lst,\
+                                                    slot_label_lst=self.slot_label_lst)
+        self.model.to(self.device)
+        self.model.eval()
         self.slot_label_map = {i: label for i, label in enumerate(self.slot_label_lst)}
 
         self.pad_token_label_id = self.args.ignore_index
@@ -50,7 +52,9 @@ class NLUprocess:
 
         self.softmax = nn.Softmax()
 
-    def inference(self, lines):
+        self.mapper = Mapper('./kb/database.json')    
+
+    def inference(self, lines,use_kb=False):
         dataset = convert_input_file_to_tensor_dataset(lines,\
                                                         self.args,\
                                                         self.tokenizer,\
@@ -97,12 +101,54 @@ class NLUprocess:
                                     "prop": self.softmax(torch.from_numpy(intent_preds_cof[i])).cpu().detach().numpy(),
                                     "entities": slot_preds_list[i]
                                     })
+        if use_kb:
+            result = result[0]
+            final_result = self.query_from_kb(lines,result)
+            final_result['intent'] = result['intent']
+            return final_result        
         return result
 
+    def query_from_kb(self,mess,result):
+        '''
+            Process single mess. Multi messages would be updated later.
+        '''
+        entities = self.get_entities(mess[0],result['entities'])
+        
+        # TODO : Ranking system
+        if result['intent'] == 'symptoms':
+            result['intent'] = 'symptom'
+        ans = self.mapper.query(result['intent'], entities)[0]
+
+        return ans
+
+    def get_entities(self,mess,preds):
+        entities = []
+        tmp = []
+        for token,label in zip(mess,preds):
+            # k = label.split()
+            # print(k)
+            if 'B' in label or 'I' in label:
+                tmp.append(token)
+            elif label=='O' and tmp != []:
+                if len(tmp) > 1:
+                    tmp= ' '.join(tmp)
+                else:
+                    tmp = tmp[0]
+                entities.append(tmp)
+                tmp = [] 
+        if tmp != []:
+            if len(tmp) > 1:
+                tmp= ' '.join(tmp)
+            else:
+                tmp = tmp[0]
+            entities.append(tmp)
+            tmp = [] 
+        return entities
+       
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, default="./phobert", help="Path of model")
-    parser.add_argument("--input_text", type=str, help="input")
+    parser.add_argument("--input_text", type=str, help="input", default='tao muốn biết thông tin về bệnh thalassemia .')
 
     args = parser.parse_args()
 

2f62aaa493fd9f4a1ab908e210b8549bdc1c85df
taindp98 <taindp98@gmail.com>
2021-04-22 13:46:02 +0700

    taindp-modify

--- modify/code/request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:5000/proc-nlu'
-results = requests.post(url, json={'mess': "tao muốn biết thông tin về bệnh thalassemia .",'_id': 1})
-
-print(results.json())

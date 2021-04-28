4c0a05a3a0c314d147323ebb0d3aabcb350e471c
taindp98 <taindp98@gmail.com>
2021-04-28 09:16:23 +0700

    fix-matching

--- code/intent_matching.py
+++ code/intent_matching.py
@@ -17,6 +17,11 @@ def matching(message):
     dict_token_intent = json.load(open(_PATH_TOKEN_INTENT,'r'))
     ## get key token from dict_token_intent
 
+    dict_result_matching = {}
+    dict_result_matching['message'] = message
+    
+
+    list_intent = []
     for item in list(dict_token_intent.keys()):
         ## field: overview, disease, symptom, serverity, prevention, ... 
         field = dict_token_intent[item]
@@ -30,13 +35,17 @@ def matching(message):
             ## calculate edit distance with THRESHOLD in config.py
             dict_eval = dist.compare_word(token_norm,mess_norm)
 
-            dict_result_matching = {}
-            dict_result_matching['message'] = message
-            dict_result_matching['confidence'] = dict_eval['partial']
+            # dict_result_matching['confidence'] = dict_eval['partial']
+            
             if dict_eval['partial'] >= INTENT_THRESHOLD:
-                dict_result_matching['intent'] = item
-                return dict_result_matching
+                # dict_result_matching['intent'] = item
+                # return dict_result_matching
+                tuple_match = (item,dict_eval['partial'])
+                if tuple_match not in list_intent:
+                    list_intent.append(tuple_match)
+    dict_result_matching['intent'] = list_intent
 
+    return dict_result_matching
 
 if __name__ == '__main__':
     message = 'cho hỏi bệnh ung thư gan có điều trị được không vậy ?'
